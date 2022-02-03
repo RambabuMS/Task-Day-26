07401dbebcd54b43b69cde4d3ff70fba52b25c26
Rambabu M <94340328+RambabuMS@users.noreply.github.com>
2022-02-03 19:13:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,109 +1,140 @@
-import './App.css';
+import PriceCard from "./pcard";
+import "./App.css";
 
-export default function App() {
-
+function App() {
+  const pricingData = [
+    {
+      packageName: "FREE",
+      price: "$0",
+      period: "month",
+      features: [
+        {
+          name: "Single User",
+          isEnabled: true,
+        },
+        {
+          name: "5GB Storage",
+          isEnabled: true,
+        },
+        {
+          name: "Unlimited Public Projects",
+          isEnabled: true,
+        },
+        {
+          name: "Community Access",
+          isEnabled: true,
+        },
+        {
+          name: "Unlimited Private Projects",
+          isEnabled: false,
+        },
+        {
+          name: "Dedicated Phone Support",
+          isEnabled: false,
+        },
+        {
+          name: "Free Subdomain",
+          isEnabled: false,
+        },
+        {
+          name: "Monthly Status Reports",
+          isEnabled: false,
+        },
+      ],
+    },
+    {
+      packageName: "PLUS",
+      price: "$9",
+      period: "month",
+      features: [
+        {
+          name: "5 Users",
+          isEnabled: true,
+          isBold: true,
+        },
+        {
+          name: "50GB Storage",
+          isEnabled: true,
+        },
+        {
+          name: "Unlimited Public Projects",
+          isEnabled: true,
+        },
+        {
+          name: "Community Access",
+          isEnabled: true,
+        },
+        {
+          name: "Unlimited Private Projects",
+          isEnabled: true,
+        },
+        {
+          name: "Dedicated Phone Support",
+          isEnabled: true,
+        },
+        {
+          name: "Free Subdomain",
+          isEnabled: true,
+        },
+        {
+          name: "Monthly Status Reports",
+          isEnabled: false,
+        },
+      ],
+    },
+    {
+      packageName: "PRO",
+      price: "$49",
+      period: "month",
+      features: [
+        {
+          name: "Unlimited Users",
+          isEnabled: true,
+          isBold: true,
+        },
+        {
+          name: "150GB Storage",
+          isEnabled: true,
+        },
+        {
+          name: "Unlimited Public Projects",
+          isEnabled: true,
+        },
+        {
+          name: "Community Access",
+          isEnabled: true,
+        },
+        {
+          name: "Unlimited Private Projects",
+          isEnabled: true,
+        },
+        {
+          name: "Dedicated Phone Support",
+          isEnabled: true,
+        },
+        {
+          name: "Free Subdomain",
+          isEnabled: true,
+          isunlimited: true,
+        },
+        {
+          name: "Monthly Status Reports",
+          isEnabled: true,
+        },
+      ],
+    },
+  ];
   return (
-<section class="pricing py-5">
-  <div class="container">
-    <div class="row">
-      <Box1 />
-      <Box2 />
-      <Box3 />
-    </div>
-  </div>
-</section>
+    <section className="pricing py-5">
+      <div className="container">
+        <div className="row">
+          {pricingData.map((obj, index) => {
+            return <PriceCard key={`C${index}`} Data={obj} />;
+          })}
+        </div>
+      </div>
+    </section>
   );
 }
 
-function Box1(){
-      
-    return (
-      <div class="col-lg-4">
-      <div class="card mb-5 mb-lg-0">
-        <div class="card-body">
-          <h5 class="card-title text-muted text-uppercase text-center">Free</h5>
-          <h6 class="card-price text-center">$0<span class="period">/month</span></h6>
-        
-          <ul class="fa-ul">
-            <li><span class="fa-li"><i class="fas fa-check"></i></span>Single User</li>
-            <li><span class="fa-li"><i class="fas fa-check"></i></span>5GB Storage</li>
-            <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Public Projects</li>
-            <li><span class="fa-li"><i class="fas fa-check"></i></span>Community Access</li>
-            <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Unlimited
-              Private Projects</li>
-            <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Dedicated
-              Phone Support</li>
-            <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Free Subdomain
-            </li>
-            <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Monthly Status
-              Reports</li>
-          </ul>
-          <div class="d-grid">
-            <a href="#" class="btn btn-primary text-uppercase">Button</a>
-          </div>
-        </div>
-      </div>
-    </div>
-    )
-}
-
-function Box2(){
-
-       return (
-        <div class="col-lg-4">
-        <div class="card mb-5 mb-lg-0">
-          <div class="card-body">
-            <h5 class="card-title text-muted text-uppercase text-center">Plus</h5>
-            <h6 class="card-price text-center">$9<span class="period">/month</span></h6>
-          
-            <ul class="fa-ul">
-              <li><span class="fa-li"><i class="fas fa-check"></i></span><strong>5 Users</strong></li>
-              <li><span class="fa-li"><i class="fas fa-check"></i></span>50GB Storage</li>
-              <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Public Projects</li>
-              <li><span class="fa-li"><i class="fas fa-check"></i></span>Community Access</li>
-              <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Private Projects</li>
-              <li><span class="fa-li"><i class="fas fa-check"></i></span>Dedicated Phone Support</li>
-              <li><span class="fa-li"><i class="fas fa-check"></i></span>Free Subdomain</li>
-              <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Monthly Status
-                Reports</li>
-            </ul>
-            <div class="d-grid">
-              <a href="#" class="btn btn-primary text-uppercase">Button</a>
-            </div>
-          </div>
-        </div>
-      </div>
-       )
-}
-
-function Box3(){
-           
-         return(
-          <div class="col-lg-4">
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title text-muted text-uppercase text-center">Pro</h5>
-              <h6 class="card-price text-center">$49<span class="period">/month</span></h6>
-              
-              <ul class="fa-ul">
-                <li><span class="fa-li"><i class="fas fa-check"></i></span><strong>Unlimited Users</strong>
-                </li>
-                <li><span class="fa-li"><i class="fas fa-check"></i></span>150GB Storage</li>
-                <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Public Projects</li>
-                <li><span class="fa-li"><i class="fas fa-check"></i></span>Community Access</li>
-                <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Private Projects</li>
-                <li><span class="fa-li"><i class="fas fa-check"></i></span>Dedicated Phone Support</li>
-                <li><span class="fa-li"><i class="fas fa-check"></i></span><strong>Unlimited</strong> Free
-                  Subdomains</li>
-                <li><span class="fa-li"><i class="fas fa-check"></i></span>Monthly Status Reports</li>
-              </ul>
-              <div class="d-grid">
-                <a href="#" class="btn btn-primary text-uppercase">Button</a>
-              </div>
-            </div>
-          </div>
-        </div>
-
-         )
-}
- 
+export default App;
